66c0f8d5837107e28e34754d12526cfffee4c2d4
Nizamuddin1N <159190139+Nizamuddin1N@users.noreply.github.com>
2025-11-02 23:45:48 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
     plugins: [react()],
 <<<<<<< HEAD
-    server: { proxy: { "/api": "https://portfolio-unfu.onrender.com/" } }
+    server: { proxy: { "/api": "https://portfolio-2-7lu4.onrender.com" } }
 })
 =======
-    server: { proxy: { "/api": "https://portfolio-unfu.onrender.com" } }
+    server: { proxy: { "/api": "https://portfolio-2-7lu4.onrender.com" } }
 })
 >>>>>>> 42be2cfa5b3fb7415a69041837459ea6471be488
